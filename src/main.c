f0e54a6bf7efc4ccb693d61f981bfa8c674b2a16
Your Name <you@example.com>
2022-01-24 15:16:03 +0200

    couibhdopifbhodifb

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
  * @copyright Copyright (c) 2022
  * 
  */
-#pragma once
 #include <stdio.h>
 #include <string.h>
 
@@ -26,11 +25,16 @@
 #include <wifi_provisioning/manager.h>
 #include <wifi_provisioning/scheme_ble.h>
 
+static const *TAG = "Unimot";
+#define DEVICE_NAME UNIMOT_DEVICE_NAME  //device name
+
 /* Signal Wi-Fi events on this event-group */
-const int WIFI_CONNECTED_EVENT = BIT0;
+const int WIFI_CONNECTED_BIT = BIT0;
+const int WIFI_FAIL_BIT = BIT1;
 static EventGroupHandle_t wifi_event_group;
 
-#include "provisioning.h"
+static int retry_num = 0;  //how many times to retry to connect to AP
+
 
 /**
  * @brief initalizing non volatile storage of program
@@ -50,17 +54,8 @@ void init_nvs()
     }
 }
 
-/* Event handler for catching system events */
-static void event_handler(void* arg, esp_event_base_t event_base,
-                          int32_t event_id, void* event_data)
+void init_internet()
 {
-
-}
-
-void app_main() 
-{
-    init_nvs(); //init storage of program
-
     /* Initialize TCP/IP */
     ESP_ERROR_CHECK(esp_netif_init());
     /* Initialize the event loop */
@@ -76,10 +71,105 @@ void app_main()
     esp_netif_create_default_wifi_sta();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+}
 
+/**
+ * @brief initalizing provisioning service
+ * 
+ */
+void init_prov()
+{
     /* Configuration for the provisioning manager */
     wifi_prov_mgr_config_t config = {
         .scheme = wifi_prov_scheme_ble, //using ble to provision device
         .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM //I don't need to use bluetooth in main app after prov
     };
+
+    /* Initialize provisioning manager with the
+     * configuration parameters set above */
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
+
+    bool provisioned = false;
+    /* Let's find out if the device is provisioned */
+    ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
+}
+
+/**
+ * @brief init wifi station
+ * 
+ */
+static void wifi_init_sta(void)
+{
+    /* Start Wi-Fi in station mode */
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+/* Event handler for catching system events */
+static void event_handler(void* arg, esp_event_base_t event_base,
+                          int32_t event_id, void* event_data)
+{
+    if (event_base == WIFI_PROV_EVENT) {
+        switch (event_id) {
+            case WIFI_PROV_START:
+                ESP_LOGI(TAG, "Provisioning started");
+                break;
+            case WIFI_PROV_CRED_RECV: {
+                wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
+                ESP_LOGI(TAG, "Received Wi-Fi credentials"
+                         "\n\tSSID     : %s\n\tPassword : %s",
+                         (const char *) wifi_sta_cfg->ssid,
+                         (const char *) wifi_sta_cfg->password);
+                break;
+            }
+            case WIFI_PROV_CRED_FAIL: {
+                wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *)event_data;
+                ESP_LOGE(TAG, "Provisioning failed!\n\tReason : %s"
+                         "\n\tPlease reset to factory and retry provisioning",
+                         (*reason == WIFI_PROV_STA_AUTH_ERROR) ?
+                         "Wi-Fi station authentication failed" : "Wi-Fi access-point not found");
+                break;
+            }
+            case WIFI_PROV_CRED_SUCCESS:
+                ESP_LOGI(TAG, "Provisioning successful");
+                break;
+            case WIFI_PROV_END:
+                /* De-initialize manager once provisioning is finished */
+                wifi_prov_mgr_deinit();
+                break;
+            default:
+                break;
+        }
+    }
+    //if event is wifi station started than connect to wifi AP
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect(); 
+    } 
+    //if event is got IP (device is connected to AP)
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) 
+    {   
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "Connected with IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+        /* Signal main application to continue execution */
+        retry_num = 0;
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_BIT);
+    } 
+    //if event is device disconnected from AP
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) 
+    {
+        if (retry_num < 5)
+         {
+            esp_wifi_connect();
+            retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(wifi_event_group, WIFI_FAIL_BIT);
+        }
+}
+
+void app_main() 
+{
+    init_nvs(); //init storage of program
+    init_internet();    //init internet
 }
